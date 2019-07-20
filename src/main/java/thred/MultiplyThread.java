c01710d6121b92748c0716abe1923999dff83f75
chenchaocom <1845360542@qq.com>
2019-07-20 16:40:41 +0800

    添加注释

--- src/main/java/thred/MultiplyThread.java
+++ src/main/java/thred/MultiplyThread.java
@@ -16,11 +16,16 @@ public class MultiplyThread {
         Thread myThred2 = new Thread(new MyThred2());
         myThred2.start();
 
-        FutureTask<String> futureTask = new FutureTask<String>(new MyThread3());
+        FutureTask<String> stringFutureTask = new FutureTask<String>(new Callable<String>() {
+            public String call() throws Exception {
+                return "aa";
+            }
+        });
 
-        Thread thread3 = new Thread(futureTask);
+
+        Thread thread3 = new Thread(stringFutureTask);
         thread3.start();
-        System.out.println(futureTask.get());
+        System.out.println(stringFutureTask.get());
 
 
     }
@@ -41,6 +46,14 @@ class MyThred2 implements Runnable{
     }
 }
 
+/**
+ * 1.继承Thread本质上也是实现Runnable接口。
+
+ 2.实现Callable接口的任务线程能返回执行结果，而实现Runnable接口的线程不能返回结果
+
+ 3.使用Callable的方式时候需要用FutureTask<>进行包装（适配），FutureTask实现了Runnable和Future，
+ 通过future里面的get()方法来得到返回值。需要注意的是get()方法是阻塞的，就是说取不到值会使主线程一直等待。
+ */
 //实现Callable接口实现多线程
  class MyThread3 implements Callable<String>{
     public String call() throws Exception {
